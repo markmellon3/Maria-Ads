e338232a7e276388bb363d0d797d3abba5666a1c
markmellon3 <119710592+markmellon3@users.noreply.github.com>
2026-09-13 10:44:36 +0300

    Update ClickTracker endpoint URL

--- assets/ad-network/click-tracker.js
+++ assets/ad-network/click-tracker.js
@@ -1,7 +1,7 @@
 export class ClickTracker {
  constructor() {
   // IMPORTANT: Replace with your actual deployed Cloud Function URL
-  this.endpoint = 'https://us-central1-YOUR_PROJECT_ID.cloudfunctions.net/recordClick';
+ this.endpoint = 'https://us-central1-maria-ad.cloudfunctions.net/recordClick';
  }
  
  attachClickEvent(element, clickToken, destinationUrl) {
